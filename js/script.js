d83a8dd8d4c51e78bda09225f454986047da310a
Yenuka Perera <yenuka@evotec.software>
2025-01-21 17:56:21 +0530

    New CV Added

--- js/script.js
+++ js/script.js
@@ -73,7 +73,7 @@ ScrollReveal().reveal('.home-content p , .about-content',{origin:'right'});
 /*==================== typed js ====================*/
 
 const typed = new Typed('.multiple-text',{
-    strings:['Software Engineer','ASP.NET Developer','Full Stack Developer','IT Support Specialist' , 'Multimedia Designer'],
+    strings:['Software Engineer','ASP.NET Developer','IT Support Specialist' , 'Multimedia Designer'],
     typeSpeed:100,
     backSpeed:100,
     backDelay:1000,
